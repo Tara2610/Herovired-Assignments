870094d5309ac4c820772d0ad778f37911a502ce
Tara2610 <130962900+Tara2610@users.noreply.github.com>
2023-07-23 20:50:27 +0530

    Update FSDAssignment.java

--- FSDAssignment.java
+++ FSDAssignment.java
@@ -332,8 +332,49 @@ class Solution {
 }
 
 // Question 8: Java Stack
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+class Solution{
+	
+	public static void main(String []argh){
+        public static boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i=0; i<s.length();++i){
+            if (s.charAt(i) == '(') stack.push('(');
+            else if (s.charAt(i) == '{') stack.push('{');
+            else if (s.charAt(i) == '[') stack.push('[');
+            else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) return false;
+                if (stack.pop() != '(') return false;
+            }
+            else if (s.charAt(i) == '}') {
+                if (stack.isEmpty()) return false;
+                if (stack.pop() != '{') return false;
+            }
+            else if (s.charAt(i) == ']') {
+                if (stack.isEmpty()) return false;
+                if (stack.pop() != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        Stack<Character> stack = new Stack<Character>();
+        Scanner sc = new Scanner(System.in);
+        String line;
+        while (sc.hasNextLine()){
+            line = sc.nextLine();
+            if (isBalanced(line)) System.out.println("true");
+            else System.out.println("false");
+        }
+    }
+}
 
 // Question 9: Java Dequeue
+
 // Question 10: Java Priority Queue
 // Question 11: Swap Nodes in Pairs
 // Question 12: Reverse Nodes in k-Group
